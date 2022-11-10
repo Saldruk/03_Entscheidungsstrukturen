1f8f2599f27a55190daa1fcc2875ed9e1337ca54
Saldruk <Divermeik@gmail.com>
2022-11-10 09:20:07 +0100

    Update if

--- script_01.js
+++ script_01.js
@@ -14,9 +14,20 @@ isJohnOlder = (ageJohn > ageMark);
 isJohnEqual = (ageJohn == ageMark);
 
 // Ausgabe
-// console.log("ageJohn: " + ageJohn);
-// console.log("ageMark: " + ageMark);
-// console.log("isJohnOlder: " + isJohnOlder);
-// console.log("isJohnEqual: " + isJohnEqual);
+// console.log("age John: " + ageJohn);
+// console.log("age Mark: " + ageMark);
+// console.log("is John Older: " + isJohnOlder);
+// console.log("is John Equal: " + isJohnEqual);
 // console.log("------------------");
 
+/********* IF ********/
+// TINA  --> there is no alternitive!
+// entweder Ja oder nix .... alternativlos
+
+// if(true)
+// if(false)
+//if(isJohnOlder)
+if(ageJohn > ageMark)
+{
+    console.log("John ist älter");
+}
